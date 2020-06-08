c64cf5d75e8510d5b660d54e12b954ed2046332c
StephenWhelan <62213003+StephenWhelan@users.noreply.github.com>
2020-06-08 14:07:22 -0400

    added health tips option to menu instead of slideshow

--- app/src/main/java/com/example/covidcapstone/ui/health_tips/HealthTipsViewModel.kt
+++ app/src/main/java/com/example/covidcapstone/ui/health_tips/HealthTipsViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.covidcapstone.ui.slideshow
+package com.example.covidcapstone.ui.health_tips
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SlideshowViewModel : ViewModel() {
+class HealthTipsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = "This is health tips Fragment"
     }
     val text: LiveData<String> = _text
 }
