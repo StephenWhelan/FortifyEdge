672294c237547b2d2c04e68dac5364832f065992
StephenWhelan <62213003+StephenWhelan@users.noreply.github.com>
2020-07-21 16:49:57 -0400

    Revert "Revert "Merge branch 'TwitterTesting'""
    This reverts commit 135cb3bec38457d3197bc70b8b4b92bb5987c636.

---
+++ app/src/main/java/com/example/covidcapstone/TwitterAdapter.kt
@@ -0,0 +1,72 @@
+package com.example.covidcapstone
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.uml.covidcapstone.R
+
+import java.util.*
+
+
+class  TwitterAdapter(
+    context: Context,
+    al_newslist: ArrayList<String>
+) :
+    ArrayAdapter<String?>(context, R.layout.adapter_layout, al_newslist as List<String?>) {
+    //var context: Context
+    var viewHolder: ViewHolder? = null
+    var al_newslist = ArrayList<String>()
+    override fun getCount(): Int {
+        Log.e("ADAPTER LIST SIZE", al_newslist.size.toString() + "")
+        return al_newslist.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return position
+    }
+
+    override fun getViewTypeCount(): Int {
+        return if (al_newslist.size > 0) {
+            al_newslist.size
+        } else {
+            1
+        }
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(
+        position: Int,
+        convertView: View?,
+        parent: ViewGroup
+    ): View {
+        var convertView = convertView
+        if (convertView == null) {
+            viewHolder = ViewHolder()
+            convertView =
+                LayoutInflater.from(getContext()).inflate(R.layout.adapter_layout, parent, false)
+            viewHolder!!.tv_name =
+                convertView!!.findViewById<View>(R.id.tv_text) as TextView
+            convertView.tag = viewHolder
+        } else {
+            viewHolder = convertView.tag as ViewHolder
+        }
+        viewHolder!!.tv_name!!.text = al_newslist[position]
+        return convertView!!
+    }
+
+    class ViewHolder {
+        var tv_name: TextView? = null
+    }
+
+    init {
+        this.al_newslist = al_newslist
+        //this.context = context
+    }
+}
---
+++ app/src/main/java/com/example/covidcapstone/ui/notifications/TwitterAccess.kt
@@ -0,0 +1,8 @@
+package com.uml.covidcapstone.ui.notifications
+ object TwitterAccess{
+    const val CONSUMER_KEY =
+            "5eniObScyYBjs6ToarKe8agZM"
+    const val CONSUMER_SECRET = "IdmJQu88cj3caKGGg7USnn7sgTkzEhHeFQckwJdlysvujVyvvR"
+    const val TwitterTokenURL = "https://api.twitter.com/oauth2/token"
+    const val TwitterStreamURL = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name="
+}
